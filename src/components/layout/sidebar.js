6b340207d95c236c54a634586c778f57da92b0c4
joseph <jeh26033@email.vccs.edu>
2022-10-25 10:54:37 -0500

    updated email address
    updated email address in sidebar

--- src/components/layout/sidebar.js
+++ src/components/layout/sidebar.js
@@ -7,7 +7,7 @@ function Sidebar() {
 	      <span className="sideBarText">.find-me</span>
 	      <ul className="sideBarLinks">
 	        <li className="sideBarLinksLi"><a href="https://www.linkedin.com/in/joseph-harskamp/">linkedIn</a></li>
-	        <li className="sideBarLinksLi"><a href="mailto:josephharskampdealeron@gmail.com">email </a></li>
+	        <li className="sideBarLinksLi"><a href="mailto:josephharskamp@gmail.com">email </a></li>
 	        <li className="sideBarLinksLi"><a href="https://github.com/jeh26033"> github</a> </li>
 	      </ul>
 	    </div>
